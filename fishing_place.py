fc04744a76927033b52a7ea6d64973db30c8aa66
ingoo <web7722@gmail.com>
2020-09-01 15:10:23 +0900

    lostark macro

--- fishing_place.py
+++ fishing_place.py
@@ -1,1 +1,24 @@
-print('Hello world!')
+import mouse as mo
+import keyboard as key
+import pyautogui as pg
+
+#마우스 포인트저장 함수
+def getMousePoint(n):
+    print('F12 키를 눌러 낚시할 포인트를 저장하세요.')
+    while True:
+        if key.is_pressed('F12') == True:
+            print('point ',n,' Catched')
+            return mo.get_position()
+    
+def fishMovePoint(currentcursor):
+    x = currentcursor[0]
+    y = currentcursor[1]
+    pg.move(x,y,0.1)
+    pg.press('w')
+    # 이미지 체크후 다시 w누르기
+
+if __name__ == "__main__":
+    pointLoop = 3
+    n = 1
+    currentcursor = getMousePoint(n) # 마우스 위치 저장
+    fishMovePoint(currentcursor) # 마우스 위치 이동
